86460951f85429fd2a726a2b595d09a8eace6cf8
Soeren Twietmeyer <st.twietmeyer@freenet.de>
2023-11-22 17:00:23 +0000

    begin modelling + increasing speed of matrix gen

--- assignment 1/code/assignment_functions.R
+++ assignment 1/code/assignment_functions.R
@@ -18,3 +18,19 @@ trend_plot <- function(x_values){
     geom_hex() + geom_smooth(method = "glm", formula = y ~ x + x^2 + x^3) + ylim(1,5)
   return(plot)
 }
+
+shrinkage_estimator_computation <- function(lasso, trng_data_x_vals, trng_data_y_vals, test_data_x_vals, test_data_y_vals) {
+  cv_out <- cv.glmnet(as.matrix(trng_data_x_vals), as.matrix(trng_data_y_vals), alpha = lasso, nfolds = 3) # adapt the number of folds
+  cv_out_plot <- plot(cv_out)
+  lambda_cv <- cv_out$lambda.min
+  
+  #Re-Estimate Ridge with lambda chosen by Cross validation
+  model<-glmnet(trng_data_x_vals, data_train_y, alpha = lasso, lambda = lambda_cv, thresh = 1e-12) # what does the thresh mean?
+  
+  #Fit on Test Data
+  predictions <- predict(ridge_model, s = lambda_ridge_cv, newx = as.matrix(test_data_x_vals))
+  mse <- mean((ridge_predictions - test_data_y_vals) ^ 2)
+  
+  results <- list(cv_out, cv_out_plot, lambda_cv, model, predictions, mse)
+  return(results)
+}
